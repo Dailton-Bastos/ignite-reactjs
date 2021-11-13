465419a4e3db9870adbaebc07e5ebdf019aed207
Dailton Bastos <dailtonbastos@gmail.com>
2021-11-13 15:43:01 -0300

    01 - fundamentos do Next.js - componentes e paginas

---
+++ chapter-III/01-fundamentos-do-next.js/02-componentes-e-paginas/src/pages/index.tsx
@@ -0,0 +1,38 @@
+import Head from 'next/head';
+import Image from 'next/dist/client/image';
+
+import { SubscribeButton } from '../components/SubscribeButton';
+
+import styles from './home.module.scss';
+
+export default function Home() {
+	return (
+		<>
+			<Head>
+				<title>Home | ig.news</title>
+			</Head>
+
+			<main className={styles.contentContainer}>
+				<section className={styles.hero}>
+					<span>👏 Hey, welcome</span>
+					<h1>
+						News about <br /> the <span>React</span> world
+					</h1>
+
+					<p>
+						Get acess to all the publications <span>for $9.90 month</span>
+					</p>
+
+					<SubscribeButton />
+				</section>
+
+				<Image
+					src="/images/avatar.svg"
+					alt="Girl coding"
+					height={520}
+					width={334}
+				/>
+			</main>
+		</>
+	);
+}

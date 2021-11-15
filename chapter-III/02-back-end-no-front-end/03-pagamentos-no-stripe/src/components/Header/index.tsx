c874ae49b6a87d11d8ed528b2973be1e2349b706
Dailton Bastos <dailtonbastos@gmail.com>
2021-11-14 23:38:56 -0300

    02 - back-end no front-end - pagamentos no stripe

---
+++ chapter-III/02-back-end-no-front-end/03-pagamentos-no-stripe/src/components/Header/index.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { SignInButton } from '../SignInButton';
+
+import styles from './styles.module.scss';
+
+export const Header = () => {
+	return (
+		<header className={styles.headerContainer}>
+			<div className={styles.headerContent}>
+				<Image
+					src="/images/logo.svg"
+					alt="ig.news"
+					width={108.45}
+					height={30.27}
+				/>
+
+				<nav>
+					<Link href="/">
+						<a className={styles.active}>Home</a>
+					</Link>
+
+					<Link href="/posts">
+						<a>Posts</a>
+					</Link>
+				</nav>
+
+				<SignInButton />
+			</div>
+		</header>
+	);
+};
